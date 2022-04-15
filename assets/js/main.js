ea77f9d88ba86f3ff6d3e8f197445374b9de537c
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-15 12:55:07 +0200

    Add Bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
-/* 
+/*  TRACCIA
     Viene fornito un file js in cui è definito un array di oggetti che rappresentano i membri del team.
     Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
-    
+
+    MILESTONE 1
+    stampare su console le informazioni di nome, ruolo e la stringa della foto
     
     MILESTONE 2 (se ce la fai falla entro le 18 altrimenti puoi consegnare anche dopo le 18 - easy):
     stampare le stesse informazioni su DOM sottoforma di stringhe
@@ -47,16 +49,20 @@ const team = [{
 /* MILESTONE 1
     stampare su console le informazioni di nome, ruolo e la stringa della foto
 */
+
+
 // stampare ogni singolo oggetto dell'array di oggetti tramite un ciclo
-for (let i = 0; i < team.length; i++) {
-    const teamElement = team[i];
+/* 
+    for (let i = 0; i < team.length; i++) {
+        const teamElement = team[i];
 
-    console.log(teamElement)
-    console.log(`Nome: ${teamElement.name}`)
-    console.log(`Ruolo: ${teamElement.role}`)
-    console.log(`Foto: ${teamElement.image}`)
+        // console.log(teamElement)
+        // console.log(`Nome: ${teamElement.name}`)
+        // console.log(`Ruolo: ${teamElement.role}`)
+        // console.log(`Foto: ${teamElement.image}`)
+    } 
+*/
 
-}
 
 
 /* MILESTONE 2
@@ -67,30 +73,85 @@ for (let i = 0; i < team.length; i++) {
     organizzare i singoli membri in card/schede
 */
 
+/*  SENZA FUNZIONI 
 
 // selezionare l'elemento della dom dove voglio stampare le informazioni
-const userRowElement = document.querySelector(".row")
+    const userRowElement = document.querySelector(".row")
     //console.log(userRowElement)
-
 for (let i = 0; i < team.length; i++) {
     const teamElement = team[i];
     // crea degli elementi della dom per ogni singolo utente 
     const userCardElement = `
-<div class="col">
+    <div class="col">
+        <div class="card border-0">
+            <div class="top_card">
+                <img class="img-fluid" src="./assets/img/${teamElement.image}" alt="dipendente team">
+            </div>
+            <!-- /.top_card -->
+            <div class="bottom_card d-flex flex-column align-items-center">
+                <div class="role mt-2 text-center">
+                    <h5>
+                        ${teamElement.role}
+                    </h5>
+                </div>
+                <div class="name text-center">
+                    <h6>
+                        ${teamElement.name}
+                    </h6>
+                </div>
+            </div>
+            <!-- /.bottom_card -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.col -->
+    `;
+
+    // console.log(userCardElement)
+    userRowElement.insertAdjacentHTML("beforeend", userCardElement);
+    // ad ogni giro deve stampare a schermo le info di ogni singolo teamElement
+};
+*/
+
+/* CON FUNZIONE*/
+
+/**
+ * ## generatore di team card nella dom 
+ * @param {array} array_object array di oggetti da stampare 
+ * @param {string} css_selector  selettore della dom
+ */
+function createTeamElementDom(array_object, css_selector) {
+    // selezionare l'elemento della dom dove voglio stampare le informazioni
+    const userRowElement = document.querySelector(css_selector);
+    //pulire l'elemento quando lo si rigenera
+    userRowElement.innerHTML = ""
+        //console.log(userRowElement)
+    for (let i = 0; i < array_object.length; i++) {
+        const teamElement = array_object[i];
+
+        const userCardElement = generateMemberMarkup(teamElement)
+
+        userRowElement.insertAdjacentHTML("beforeend", userCardElement);
+
+    };
+}
+
+function generateMemberMarkup(member_object) {
+    return `<div class="col">
     <div class="card border-0">
         <div class="top_card">
-            <img class="img-fluid" src="./assets/img/${teamElement.image}" alt="dipendente team">
+            <img class="img-fluid" src="./assets/img/${member_object.image}" alt="dipendente team">
         </div>
         <!-- /.top_card -->
         <div class="bottom_card d-flex flex-column align-items-center">
             <div class="role mt-2 text-center">
                 <h5>
-                    ${teamElement.role}
+                    ${member_object.role}
                 </h5>
             </div>
             <div class="name text-center">
                 <h6>
-                    ${teamElement.name}
+                    ${member_object.name}
                 </h6>
             </div>
         </div>
@@ -99,8 +160,114 @@ for (let i = 0; i < team.length; i++) {
     <!-- /.card -->
 </div>
 <!-- /.col -->
-`;
-    // console.log(userCardElement)
-    userRowElement.insertAdjacentHTML("beforeend", userCardElement);
-    // ad ogni giro deve stampare a schermo le info di ogni singolo teamElement
-};
+`
+}
+
+
+createTeamElementDom(team, ".row")
+
+
+
+/* BONUS NASCOSTO */
+// selezionare il form
+
+const formElement = document.querySelector('form');
+// aggiungi event listener
+formElement.addEventListener('submit', function(event) {
+    // previenire refresh della pagina
+    event.preventDefault()
+        // prendere i valori dei campi compixlati
+    console.log(event);
+
+
+    // prendere il valore dei campi compilati
+    const name = event.target.name.value
+    const role = event.target.role.value
+    const image = event.target.image.value
+        // usare i valori presi sopra per creare un nuovo oggetto
+    const newMember = {
+        name,
+        role,
+        image
+    }
+
+    console.log(newMember);
+
+    // pushare il nuovo oggetto nell'area team
+    team.push(newMember)
+    console.log(team)
+
+    // rigenerare le card con i membri 
+    createTeamElementDom(team, ".row")
+
+    // ripulire gli input prima dell'invio di un form
+    document.getElementById("name").value = ""
+    document.getElementById("role").value = ""
+    document.getElementById("image").value = ""
+
+    // impedire all'utente di inviare form senza compilare i campi tramite required negli input dell'html
+
+    // mostra un allerta che scomprare dopo 2 secondi
+
+    const form_alert = document.querySelector(".alert")
+    form_alert.style.display = "block";
+    form_alert.innerHTML = `Hai inserito ${name} con successo`;
+    // crea un setTimout per far sparire il pop up generato
+    setTimeout(() => {
+        form_alert.style.display = "none";
+    }, 4000)
+})
+
+
+
+
+
+
+/* SOLUZIONE FABIO 
+
+    //MILESTONE 1
+    for (let i = 0; i < team.length; i++) {
+        const member = team[i];
+        // console.log(member.name, member.role, member.image)
+        // const member_name = member.name
+        // const member_role = member.role
+        // const member_image = member.image
+        // console.log(member_name, member_role, member_image)
+
+        for (const key in member) {
+            console.log(`${key}: ${member[key]}`)
+        }
+
+    } 
+
+    // MILESTONE 2
+    for (let i = 0; i < team.length; i++) {
+        const member = team[i];
+        const memberMarkup = const userCardElement = `
+        <div class="col">
+            <div class="card border-0">
+                <div class="top_card">
+                    <img class="img-fluid" src="./assets/img/${teamElement.image}" alt="dipendente team">
+                </div>
+                <!-- /.top_card -->
+                <div class="bottom_card d-flex flex-column align-items-center">
+                    <div class="role mt-2 text-center">
+                        <h5>
+                            ${teamElement.role}
+                        </h5>
+                    </div>
+                    <div class="name text-center">
+                        <h6>
+                            ${teamElement.name}
+                        </h6>
+                    </div>
+                </div>
+                <!-- /.bottom_card -->
+            </div>
+            <!-- /.card -->
+        </div>
+        <!-- /.col -->
+        `;
+        userRowElement.insertAdjacentHTML("beforeend", memberMarkup)
+    }
+*/
